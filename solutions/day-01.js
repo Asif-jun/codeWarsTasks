47953ef10ba471114ddd040c5062b229e837fdff
asifabbasov <aassifabbasov@gmail.com>
2025-09-10 23:22:02 +0400

    CodeWars Day 2

--- solutions/day-01.js
+++ solutions/day-01.js
@@ -1,4 +1,11 @@
 // Создайте функцию, которая принимает целое число в качестве аргумента и возвращает «Even» для четных чисел или «Odd» для нечетных чисел.
 
 const evenOrOdd = n => (n % 2 === 0 ? 'Even' : 'Odd')
-console.log(evenOrOdd(4))
+// console.log(evenOrOdd(4))
+
+// Этот код не выполняется должным образом. Попробуйте выяснить причину.
+function multiply(a, b) {
+  return a * b
+}
+
+console.log(multiply(2, 2))
